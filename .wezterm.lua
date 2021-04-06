5ef81d2859fb6df3a155c33a6915ca77d22473e1
Gus Wynn <guswynn@gmail.com>
2021-04-06 08:51:59 -0700

    small improvements to wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -29,12 +29,14 @@ return {
       font = font_with_fallback("Iosevka Term Extralight"),
     },
   },
-   -- Better for my monitor
+  -- Better for my monitor
   dpi = 110.0,
-  font_size = 12,
- 
+  font_size = 11,
+
   bold_brightens_ansi_colors = true,
-  font_antialias = "Greyscale",
+  font_antialias = "Subpixel",
+  -- no ligatures
+  harfbuzz_features = {"calt=1", "clig=0", "liga=0"},
 
   -- color_scheme = "Galizur",
   -- color_scheme = "Wryan",
@@ -42,4 +44,31 @@ return {
   -- color_scheme = "synthwave",
   -- color_scheme = "Harper",
   color_scheme = "3024 Night",
+
+  -- Middle mouse button pastes the primary selection.
+  mouse_bindings = {
+    -- See https://github.com/wez/wezterm/issues/119
+    --
+    -- Change the default click behavior so that it only selects
+    -- text and doesn't open hyperlinks
+    {
+      event={Up={streak=1, button="Left"}},
+      mods="NONE",
+      action=wezterm.action{CompleteSelection="Clipboard"},
+    },
+    -- and make CTRL-Click open hyperlinks
+    {
+      event={Up={streak=1, button="Left"}},
+      mods="CTRL",
+      action="OpenLinkAtMouseCursor",
+    },
+  },
+
+  -- https://wezfurlong.org/wezterm/config/lua/config/selection_word_boundary.html#selection_word_boundary
+  -- include punctuation
+  selection_word_boundary = " \t\n{}[]()\"'`,;:",
+
+  -- if needed: https://wezfurlong.org/wezterm/hyperlinks.html
+  -- hyperlink_rules = {
+  -- },
 }
